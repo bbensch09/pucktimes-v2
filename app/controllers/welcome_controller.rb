a053ed0913a35b1da3c504b54c8a5dedc26f4b82
Brian <bbensch@gmail.com>
2016-02-19 17:13:27 -0800

    styling updates for bg images; add routes for t/today and /about us

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def show
+  redirect_to article_path(Article.last) and return
+  end
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  has_attached_file :cover_photo, styles: { medium: "400x400>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :cover_photo, styles: { large: "600x400>", thumb: "120x80>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :cover_photo, content_type: /\Aimage\/.*\Z/
 
   def time_since_written
