3fb616d96d71ad49872351dd71f056d00646e752
Brian <bbensch@gmail.com>
2016-05-25 12:47:50 -0700

    custom page titles to match article titles, for FB sharing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def title(title = nil)
+    if title.present?
+      content_for :title, title
+    else
+      content_for?(:title) ? content_for(:title) : APP_CONFIG['default_title']
+    end
+  end
 end
